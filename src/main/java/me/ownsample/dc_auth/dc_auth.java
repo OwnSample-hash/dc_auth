6fa370b59af79d9ba4b164154a5c0b3fe951a03a
OwnSample <ownsample@tutanota.com>
2023-10-27 21:11:09 +0200

    Initial setups

---
+++ src/main/java/me/ownsample/dc_auth/dc_auth.java
@@ -0,0 +1,22 @@
+package me.ownsample.dc_auth;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class dc_auth extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    }
+
+}
