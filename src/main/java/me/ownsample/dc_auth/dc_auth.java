f6881503bf98b08dd1fd4f8c23be35b8673e85a5
OwnSample <ownsample@tutanota.com>
2023-10-28 15:01:14 +0200

    Added mysql db backend, cmd link

--- src/main/java/me/ownsample/dc_auth/dc_auth.java
+++ src/main/java/me/ownsample/dc_auth/dc_auth.java
@@ -1,22 +1,55 @@
 package me.ownsample.dc_auth;
 
-import net.kyori.adventure.text.Component;
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import me.ownsample.dc_auth.EventHandlers.dc_listener;
+import me.ownsample.dc_auth.cmd.Link;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class dc_auth extends JavaPlugin implements Listener {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.sql.*;
+public class dc_auth extends JavaPlugin  {
+
+    public static JDA jda;
+    public static List<Player> frozen_players = new ArrayList<Player>();
+    public Connection con;
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+        jda = JDABuilder.createDefault(getConfig().getString("token"))
+                .addEventListeners(new dc_listener(this))
+                .build();
+        try {
+            con = DriverManager.getConnection(Objects.requireNonNull(getConfig().getString("jdbc")));
+            Statement stm = con.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS auth (" +
+                    "    id INT NOT NULL PRIMARY KEY," +
+                    "    name VARCHAR(255) NOT NULL" +
+                    ");";
+            stm.executeUpdate(sql);
+            stm.close();
+        } catch (SQLException e) {
+            getLogger().severe("SQLException");
+            getLogger().severe(e.getMessage());
+        }
+        getCommand("link").setExecutor(new Link(con, this));
+        //Bukkit.getPluginManager().registerEvents(new Freeze(this), this);
     }
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    @Override
+    public void onDisable(){
+        if (con != null){
+            try {
+                con.close();
+            } catch (SQLException e) {
+                getLogger().severe("SQLException");
+                getLogger().severe(e.getMessage());
+            }
+        }
     }
-
 }
